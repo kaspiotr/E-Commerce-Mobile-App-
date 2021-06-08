c5fbae1089e61b1999702172c6efa65ca0abbd83
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-08 16:30:01 +0200

    Add feature of closing splash screen and moving to main app screen after some period of time

--- app/src/main/java/pro/kaspiotr/ecommercemobileapp/SplashActivity.kt
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/SplashActivity.kt
@@ -1,8 +1,10 @@
 package pro.kaspiotr.ecommercemobileapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 
@@ -20,5 +22,15 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                // Launch the Main Activity
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            },
+            2500
+        )
     }
 }
