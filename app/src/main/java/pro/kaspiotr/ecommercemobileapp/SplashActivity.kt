e25c2b770f115e7cf32f52ca59b8d0ecd66f45ff
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-08 16:20:30 +0200

    Add splash screen displayed on whole screeen

---
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/SplashActivity.kt
@@ -0,0 +1,17 @@
+package pro.kaspiotr.ecommercemobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
