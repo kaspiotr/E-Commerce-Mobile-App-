877c90d27295af8fa67730e5b05bcc0dc116e05a
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-08 19:58:42 +0200

    Add login screen

--- app/src/main/java/pro/kaspiotr/ecommercemobileapp/activities/LoginActivity.kt
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/activities/LoginActivity.kt
@@ -1,18 +1,16 @@
 package pro.kaspiotr.ecommercemobileapp.activities
 
-import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 import pro.kaspiotr.ecommercemobileapp.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
 
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -23,15 +21,5 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-
-        @Suppress("DEPRECATION")
-        Handler().postDelayed(
-            {
-                // Launch the Main Activity
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                finish()
-            },
-            1500
-        )
     }
 }
