0fc60f5521f24167711e1ff80f44b79980195848
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-08-09 00:44:10 +0200

    Set up action bar in setting screen

--- app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/SettingsActivity.kt
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/SettingsActivity.kt
@@ -11,6 +11,7 @@ class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setUpActionBar()
     }
 
     private fun setUpActionBar() {
