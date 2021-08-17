994b1daa5db904ea91a70e6606523eac6cbb7add
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-08-17 19:20:35 +0200

    Design screen for adding product
       * create screen layout
       * add feature of going back from activity by clicking
         back arrow

--- app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/AddProductActivity.kt
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/AddProductActivity.kt
@@ -2,11 +2,25 @@ package pro.kaspiotr.ecommercemobileapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_product.*
 import pro.kaspiotr.ecommercemobileapp.R
 
 class AddProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_add_product_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
     }
 }
