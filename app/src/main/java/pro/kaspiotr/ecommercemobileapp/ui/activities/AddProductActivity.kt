f03749f295a5c4a90c4dec3079b59708d2de64f7
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-08-29 17:59:46 +0200

    Implement setting product image on add product screen
       * check if user gave the rights to use permissions
       * change icon from adding new image (camera) to edit image
         (pencil) if the user have already selected an image before

--- app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/AddProductActivity.kt
+++ app/src/main/java/pro/kaspiotr/ecommercemobileapp/ui/activities/AddProductActivity.kt
@@ -1,15 +1,29 @@
 package pro.kaspiotr.ecommercemobileapp.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_product.*
+import kotlinx.android.synthetic.main.activity_user_profile.*
 import pro.kaspiotr.ecommercemobileapp.R
+import pro.kaspiotr.ecommercemobileapp.utils.Constants
+import pro.kaspiotr.ecommercemobileapp.utils.GlideLoader
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+class AddProductActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
         setupActionBar()
+
+        iv_add_update_product.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -22,5 +36,64 @@ class AddProductActivity : AppCompatActivity() {
         }
 
         toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_update_product -> {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Constants.showImageChooser(this@AddProductActivity)
+                    } else {
+                        /*
+                        Requests permissions to be granted to this application. These permissions must
+                        be requested in the manifest, they should not be granted to the app, and they
+                        should have protection level.
+                         */
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Constants.showImageChooser(this)
+        } else {
+            Toast.makeText(
+                this, resources.getString(R.string.read_storage_permission_denied),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    iv_add_update_product.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+                    val selectImageFileUri = data.data!! //location of file
+                    try {
+                        GlideLoader(this).loadUserPicture(selectImageFileUri, iv_product_image)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // A log is printed when user close or cancel the image selection.
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
     }
 }
